ac270db221c34417c77a970146517f04da2dd030
Muntasir <muntasirsunny05@gmail.com>
2024-04-12 20:20:23 +0600

    Modifying list

---
+++ C# Coding Excercise/ModifyList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Coding_Excercise
+{
+    internal class ModifyList
+    {
+        static void Main(string[] args)
+        {
+            var fruits = new List<string> {"mango", "litchi", "banana", "coconut"};
+            fruits.Add("jackfruit");
+            fruits.Remove("litchi");
+
+            foreach (var fruit in fruits)
+            {
+                Console.WriteLine(fruit);
+            }
+        }
+    }
+}
+/*OUTPUT:
+mango
+banana
+coconut
+jackfruit*/
+
