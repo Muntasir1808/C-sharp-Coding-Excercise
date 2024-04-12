5072b9134878e2a6a37336c5048696e5cb8b9e04
Muntasir <muntasirsunny05@gmail.com>
2024-04-12 20:39:26 +0600

    List Sorting

---
+++ C# Coding Excercise/4_ListSorting.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Coding_Excercise
+{
+    internal class _4_ListSorting
+    {
+        static void Main(string[] args)
+        {
+            var fruits = new List<string> { "mango", "litchi", "banana", "coconut", "berry", "apple" };
+            fruits.Sort();
+            foreach (var fruit in fruits)
+            {
+                Console.Write($" {fruit}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
