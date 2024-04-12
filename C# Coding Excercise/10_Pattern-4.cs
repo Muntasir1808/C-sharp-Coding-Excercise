048ffaf9565e416445cadc37608478a3d6763ae9
Muntasir <muntasirsunny05@gmail.com>
2024-04-12 23:25:14 +0600

    right angle triangle pattern

---
+++ C# Coding Excercise/10_Pattern-4.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Coding_Excercise
+{
+    internal class _10_Pattern_4
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number of rows: ");
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out int rows))
+            {
+                int value = 1;
+                for (int row = 1; row <= rows; row++)
+                {
+                    for (int col = 1; col <= row; col++)
+                    {
+                        Console.Write($"{value} ");
+                        value++;
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
+/*
+    Sample Input: 
+    Enter number of rows: 4
+    Output:
+    1
+    2 3
+    4 5 6
+    7 8 9 10
+ 
+ */
