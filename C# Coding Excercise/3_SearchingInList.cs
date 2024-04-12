c9d1df22548ccde3b22ebf00fe5984dde1cc4944
Muntasir <muntasirsunny05@gmail.com>
2024-04-12 20:29:50 +0600

    Searching in list

---
+++ C# Coding Excercise/3_SearchingInList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Coding_Excercise
+{
+    internal class SearchingInList
+    {
+        static void Main(string[] args)
+        {
+            var fruits = new List<string> { "mango", "litchi", "banana", "coconut", "berry", "apple" };
+            var index = fruits.IndexOf("litchi");
+            if (index != -1)
+            {
+                Console.WriteLine($"{fruits[index]} is at index {index}");
+            }
+        }
+    }
+}
