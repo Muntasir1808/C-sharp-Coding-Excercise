af6ff96bfa4b42534dff39f2d692018f4ad1411f
Muntasir <muntasirsunny05@gmail.com>
2024-04-12 20:58:51 +0600

    fibonacci number

---
+++ C# Coding Excercise/5_FibonacciNumbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Coding_Excercise
+{
+    internal class FibonacciNumbers
+    {
+        static void Main(string[] args)
+        {
+            //Finding out fibonacci number upto 15th place
+            var fibonacciNumbers = new List<int> { 1, 1 };
+            
+            while(fibonacciNumbers.Count <= 15)
+            {
+                var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+                var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+                fibonacciNumbers.Add(previous + previous2);
+            }
+
+            foreach(var number in fibonacciNumbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+    }
+}
