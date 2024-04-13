d198228dc71c8e77c841d382733ff2ae4333446d
Muntasir <muntasirsunny05@gmail.com>
2024-04-13 11:45:27 +0600

    right side triangle

---
+++ C# Coding Excercise/20_Pattern-14.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Coding_Excercise
+{
+    internal class _20_Pattern_14
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number of rows: ");
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out int rows))
+            {              
+                for (int row = 0; row < rows; row++)
+                {                   
+                    for (int col = 1; col <= (row * 2) + 1; col++)
+                    {
+                        Console.Write($"*");
+                    }
+                    Console.WriteLine();
+
+                }
+
+                for (int row = rows-2; row >= 0; row--)
+                {
+                    for (int col = 1; col <= (row * 2) + 1; col++)
+                    {
+                        Console.Write($"*");
+                    }
+                    Console.WriteLine();
+
+                }
+
+            }
+        }
+    }
+}
+/*
+Sample Input:
+Enter number of rows: 5
+Output:
+*
+***
+*****
+*******
+*********
+*******
+*****
+***
+*
+
+ */
