245a3ab5c2435640a37cd390939359d17002a17c
Muntasir <muntasirsunny05@gmail.com>
2024-04-13 10:41:45 +0600

    lower and upper pyramid combined

---
+++ C# Coding Excercise/14_Pattern-8.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Coding_Excercise
+{
+    internal class _14_Pattern_8
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number of rows: ");
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out int rows))
+            {
+                int spaces = 0;
+                for (int row = 0; row < rows; row++)
+                {
+                    spaces = rows - row - 1;
+                    for (int space = spaces; space >= 0; space--)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int col = 1; col <= (row * 2) + 1; col++)
+                    {
+                        Console.Write($"*");
+                    }
+                    Console.WriteLine();
+                }
+                //reverse pyramid
+                for (int row = rows-2; row >= 0; row--)
+                {
+                    spaces = rows - row - 1;
+                    for (int space = 0; space <= spaces; space++)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int col = 0; col <=(row*2); col++)
+                    {
+                        Console.Write($"*");                      
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+
+    }
+}
+
+/* 
+ Sample Input:
+ Enter number of rows: 4
+    *
+   ***
+  *****
+ *******
+  *****
+   ***
+    *
+  
+ */
