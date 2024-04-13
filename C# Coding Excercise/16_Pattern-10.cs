8b96c754919e312563266bc2fea0e7009f3d23d8
Muntasir <muntasirsunny05@gmail.com>
2024-04-13 10:49:48 +0600

    Reverse pyramid with odd number of asterisk

---
+++ C# Coding Excercise/16_Pattern-10.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Coding_Excercise
+{
+    internal class _16_Pattern_10
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number of rows: ");
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out int rows))
+            {
+                int spaces = 0;
+                //reverse pyramid
+                for (int row = rows - 2; row >= 0; row--)
+                {
+                    spaces = rows - row - 1;
+                    for (int space = 0; space <= spaces; space++)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int col = 0; col <= (row * 2); col++)
+                    {
+                        Console.Write($"*");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
+/* Sample Input:
+   Enter number of rows: 5
+   Output:
+   *******
+    *****
+     ***
+      *
+ */
